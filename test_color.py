3bbbc7e0f2ba84f35f466d35c16213bc48c17156
Andre Luis Goncalves Oliveira <algo1305@gmail.com>
2014-03-31 03:36:43 -0400

    Update and rename color_test.py to test_color.py

---
+++ test_color.py
@@ -0,0 +1,12 @@
+import unittest
+
+class test_color(unittest.TestCase)
+
+def test_convertColorDepth_ErrorIfNotInColorRange(self, color):  # Checks if the color value is bigger than 255 or smaller than 0
+
+def setUp(self):
+
+	self.assertTrue(0 <= color <= 255, msg="Error: color is out of the range! (0 - 255)")
+
+if __name__ == '__main__':
+    unittest.main()
