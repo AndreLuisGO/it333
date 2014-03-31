f354e1334e33f7fa7677d031718a83960cb36ea9
Stoney Jackson <dr.stoney@gmail.com>
2014-03-31 11:24:14 -0400

    Stoney's review of test_color.py

--- test_color.py
+++ test_color.py
@@ -1,12 +1,39 @@
 import unittest
 
-class test_color(unittest.TestCase)
+import mousetrap.app.ui.color
 
-def test_convertColorDepth_ErrorIfNotInColorRange(self, color):  # Checks if the color value is bigger than 255 or smaller than 0
+class test_color(unittest.TestCase):
 
-def setUp(self):
+    # test_<unit-of-work>_<state-under-test>_<expected-behavior>
+    # test_convertColorDepth_passedTypicalRgbValues_returnTypicalHsvValue:
+    # test_convertColorDepth_badRgbValuesPassed_throwsException:
+    def test_convertColorDepth_ErrorIfNotInColorRange(self, color):
 
-	self.assertTrue(0 <= color <= 255, msg="Error: color is out of the range! (0 - 255)")
+        # SETUP
+
+        blackRgb = (0, 0, 0)
+        expectedBlackHsv = (255, 255, 0)
+
+        # EXECUTE
+
+        resultBlackHsv = color.convertColorDepth(blackRgb)
+
+        # ASSESS
+
+        self.assertTrue(0 <= resultBlackHsv[0] <= 255)
+        self.assertTrue(0 <= resultBlackHsv[1] <= 255)
+        self.assertTrue(0 == resultBlackHsv[2])
+
+        self.assertTrue(
+            resultBlackHsv == expectedBlackHsv,
+            msg="Error: result black hsv "
+                + str(resultBlackHsv)
+                + " does not match expectation "
+                + str(expectedBlackHsv)
+        )
+
+        # self.assertTrue(0 <= color <= 255, msg="Error: color is out of the range! (0 - 255)")
+    
 
 if __name__ == '__main__':
     unittest.main()
