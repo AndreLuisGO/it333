e5f4935a4f8f66270e697c1463ac9451e2f92026
Andre Luis Goncalves Oliveira <algo1305@gmail.com>
2014-04-09 02:59:34 -0400

    changing to meet actual method purpose.
    Applying changes due to misunderstood what the original method `convertColorDepth()` does.

--- test_color.py
+++ test_color.py
@@ -5,35 +5,32 @@ import mousetrap.app.ui.color
 class test_color(unittest.TestCase):
 
     # test_<unit-of-work>_<state-under-test>_<expected-behavior>
-    # test_convertColorDepth_passedTypicalRgbValues_returnTypicalHsvValue:
-    # test_convertColorDepth_badRgbValuesPassed_throwsException:
+    # This tests if the 16-bit color depth is correctly being converted to 8-bit color depth
     def test_convertColorDepth_ErrorIfNotInColorRange(self, color):
 
         # SETUP
 
-        blackRgb = (0, 0, 0)
-        expectedBlackHsv = (255, 255, 0)
+        whiteRgb16Bit = (65535,65536, 65535)
+        expectedWhite8Bit = (255, 255, 255)
 
         # EXECUTE
 
-        resultBlackHsv = color.convertColorDepth(blackRgb)
+        resultWhite8Bit = color.convertColorDepth(whiteRgb16bit)
 
         # ASSESS
 
-        self.assertTrue(0 <= resultBlackHsv[0] <= 255)
-        self.assertTrue(0 <= resultBlackHsv[1] <= 255)
-        self.assertTrue(0 == resultBlackHsv[2])
+        self.assertTrue(0 <=  expectedWhite8Bit[0] <= 255)
+        self.assertTrue(0 <=  expectedWhite8Bit[1] <= 255)
+        self.assertTrue(0 <=  expectedWhite8Bit[2] <= 255)
 
         self.assertTrue(
-            resultBlackHsv == expectedBlackHsv,
-            msg="Error: result black hsv "
-                + str(resultBlackHsv)
+            resultWhite8Bit == expectedWhite8Bit,
+            msg="Error: result white 8bit "
+                + str(resultWhite8Bit)
                 + " does not match expectation "
-                + str(expectedBlackHsv)
+                + str(expectedWhite8Bit)
         )
 
-        # self.assertTrue(0 <= color <= 255, msg="Error: color is out of the range! (0 - 255)")
-    
 
 if __name__ == '__main__':
     unittest.main()
