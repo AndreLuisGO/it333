1869d8bb16622c33146d9c9a59300c62e1657a55
Andre Luis Goncalves Oliveira <algo1305@gmail.com>
2014-04-23 02:56:57 -0400

    Update color.py

--- color.py
+++ color.py
@@ -1,23 +1,12 @@
-import unittest
-import color
-
-class test_color(unittest.TestCase):
-
-    def test_convertColorDepth_CorrectInput(self):
-
-        whiteRgb16Bit = 65535
-        expectedWhite8Bit = 255
-        resultWhite8Bit = color.convertColorDepth(self,whiteRgb16Bit)
-
-        self.assertTrue(0 <= resultWhite8Bit <= 255)
-        self.assertTrue(
-            resultWhite8Bit == expectedWhite8Bit,
-            msg="Error: result white 8bit "
-                + str(resultWhite8Bit)
-                + " does not match expectation "
-                + str(expectedWhite8Bit)
-        )
-
-
-if __name__ == '__main__':
-    unittest.main()
+    def _convertColorDepth(self, color):
+        """
+        Converts from 16 to 8 bit color depth. Necessary
+        for OpenCV functions and GDK colors to interact as the
+        former expects colors from 0-255 and the latter expects
+        0-65535.
+        
+        Arguments:
+        - self: The main object pointer
+        - color: The integer color value to convert to 0-255
+        """
+        return int((color / 65535.0) * 255)
