a498078ccd08eef4d9ef6312d59722acc5cd97d3
Andre Luis Goncalves Oliveira <algo1305@gmail.com>
2014-04-23 01:33:57 -0400

    Final test file version
    removed unused space and debugged all the errors.

--- test_color.py
+++ test_color.py
@@ -1,17 +1,16 @@
 import unittest
-
-import color  
+import color
 
 class test_color(unittest.TestCase):
 
-    def test__convertColorDepth_CorrectInput(self):
+    def test_convertColorDepth_CorrectInput(self):
 
-        whiteRgb16Bit = (65535)
-        expectedWhite8Bit = (255)
-        
-        resultWhite8Bit = color._convertColorDepth(whiteRgb16bit)
-        self.assertTrue(0 <=  resultWhite8Bit <= 255)
+        whiteRgb16Bit = 65535
+        expectedWhite8Bit = 255
 
+        resultWhite8Bit = color.convertColorDepth(self,whiteRgb16Bit)
+
+        self.assertTrue(0 <= resultWhite8Bit <= 255)
         self.assertTrue(
             resultWhite8Bit == expectedWhite8Bit,
             msg="Error: result white 8bit "
